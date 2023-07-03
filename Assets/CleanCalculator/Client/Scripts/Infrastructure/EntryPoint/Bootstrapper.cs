30e0d0049584753813a8ff3e7ea77ab6dcc815d9
Aleh <OBeketa@synergy.ru>
2023-07-04 00:32:04 +0300

    Entry point

---
+++ Assets/CleanCalculator/Client/Scripts/Infrastructure/EntryPoint/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+ using Data;
+ using Domain;
+ using Domain.Operation;
+ using UnityEngine;
+ 
+ public class Bootstrapper : MonoBehaviour
+ {
+     public SceneDataMono SceneData;
+ 
+     public void Awake()
+     {
+         IRepository repository = new PlayerPrefsRepository();
+ 
+         ServiceContext
+             .Container
+             .RegistrationSingle
+                 <IRepository>(repository);
+     }
+ }
